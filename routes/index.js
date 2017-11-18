9f4d7616feb766ee01c82feea935d365043da8f5
marcoprado17 <marco.pdsv@gmail.com>
2017-11-17 22:37:20 -0200

    Se você estiver lendo esse commit mande um email para urielmendes17@gmail.com

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var extract = require('pdf-text-extract');
+var hummus = require('hummus');
+var path = require('path');
+var fs = require('fs');
+var readline = require('readline');
 
 const connectionString = 'postgres://ivonetedb:123456@localhost:5432/ivonetedb'
 
@@ -30,9 +35,69 @@ router.get('/admin-upload', function(req, res, next) {
 
 router.post('/admin-upload', function(req, res, next) {
 	if (!req.files || !('boletos' in req.files))
-    	return res.status(400).send('Nenhum arquivo foi encontrado.');
+		return res.status(400).send('Nenhum arquivo foi encontrado.');
 
-    req.files.boletos.mv('uploaded_files/boletos.pdf');
+	req.files.boletos.mv('routes/boletos.pdf');
+
+	//#############
+	var sourcePDF = path.join(__dirname, 'boletos.pdf');
+	console.log(sourcePDF);
+ 	var outputFolder = path.join(__dirname, '/output');
+
+	extract(sourcePDF, (err, pages) => {
+		if (err) console.log(err);
+
+		var pdfWriter;
+
+		for(var i = 0; i< pages.length; i++) {
+			var indexStart = pages[i].indexOf("Pagador:",2000)+12;
+			var indexEnd = pages[i].indexOf("-",3500)-1;
+			var nome = pages[i].substring(indexStart,indexEnd);
+
+			indexStart = pages[i].indexOf("Vencimento")+136;
+			indexEnd = indexStart+10;
+
+			var date = pages[i].substring(indexStart,indexEnd);
+
+			date = date.replace(/\//g,'-');
+			var nomeArq = nome.concat(" @ ",date,".pdf");
+			console.log(nomeArq);
+
+			if(nome != ""){
+				pdfWriter = hummus.createWriter(path.join(outputFolder, nomeArq));
+				pdfWriter.appendPDFPagesFromPDF(sourcePDF, {type:hummus.eRangeTypeSpecific,specificRanges: [ [ i , i]]});
+				pdfWriter.end();	
+			}
+		}
+
+		fs.readdirSync(outputFolder).forEach(file => {
+			var mes = file.substring(file.indexOf("@")+5,file.indexOf("@")+7);
+			var ano = file.substring(file.indexOf("@")+8,file.indexOf("@")+12);
+			var nome = file.substring(0,file.indexOf("@")-1)
+    		
+			console.log("mes: " + mes);
+			console.log("ano: " + ano);
+			console.log("nome: " + nome);
+
+    		//colocar aqui os SQL
+			pg.connect(connectionString, (err, client, done) => {
+		    // Handle connection errors
+		    if(err) {
+		    	done();
+		    	console.log(err);
+		    	return res.status(500).json({success: false, data: err});
+		    }
+		    // SQL Query > Insert Data
+		    client.query('INSERT INTO bill(year, month, student_name, src) values($1, $2, $3, $4) ON CONFLICT(year, month, student_name) DO UPDATE SET src = $4 WHERE bill.year = $1 AND bill.month = $2 AND bill.student_name = $3',
+		    	[ano, mes, nome, path.join(outputFolder, file)], 
+		    	function(err, result) {
+		    		if (err) {
+		    			console.log(err);
+		    		}
+		    	});
+		    });
+		});
+	});
 
 	res.redirect('/');
 });
@@ -59,12 +124,12 @@ router.get('/student-search', function(req, res, next) {
   	pg.connect(connectionString, (err, client, done) => {
     	// Handle connection errors
     	if(err) {
-	    	done();
-	    	console.log(err);
-	    	return res.status(500).json({success: false});
+    		done();
+    		console.log(err);
+    		return res.status(500).json({success: false});
     	}
 	    // SQL Query > Select Data
-	    var query = client.query('SELECT * FROM student WHERE student.name like $1 ORDER BY student.name', ['%'+studentName+'%']);
+	    var query = client.query('SELECT student_name FROM bill WHERE student_name like $1 ORDER BY student_name', ['%'+studentName+'%']);
 	    // Stream results back one row at a time
 	    query.on('row', (row) => {
 	    	results.push(row);
@@ -75,12 +140,12 @@ router.get('/student-search', function(req, res, next) {
 	    	return res.render('student_search', {'student_name': studentName, 'results': results});
 	    });
 	});
-});
+  });
 
 router.get('/student-boletos', function(req, res, next) {
-	var studentCpf = req.param('student_cpf', null);
+	var studentName = req.param('student_name', null);
 
-	if(studentCpf === null){
+	if(studentName === null){
 		return res.status(404).send("Not found");
 	}
 
@@ -90,12 +155,12 @@ router.get('/student-boletos', function(req, res, next) {
   	pg.connect(connectionString, (err, client, done) => {
     	// Handle connection errors
     	if(err) {
-	    	done();
-	    	console.log(err);
-	    	return res.status(500).json({success: false});
+    		done();
+    		console.log(err);
+    		return res.status(500).json({success: false});
     	}
 	    // SQL Query > Select Data
-	    var query = client.query('SELECT * FROM bill, student WHERE bill.student_cpf = $1 AND bill.student_cpf = student.cpf ORDER BY bill.year DESC, bill.month DESC', [studentCpf]);
+	    var query = client.query('SELECT * FROM bill WHERE bill.student_name = $1 ORDER BY bill.year DESC, bill.month DESC', [studentName]);
 	    // Stream results back one row at a time
 	    query.on('row', (row) => {
 	    	results.push(row);
@@ -104,10 +169,10 @@ router.get('/student-boletos', function(req, res, next) {
 	    query.on('end', () => {
 	    	done();
 	    	console.log(results);
-	    	return res.render('student_boletos', {'results': results, 'student_cpf': studentCpf});
+	    	return res.render('student_boletos', {'results': results, 'student_name': studentName});
 	    });
 	});
-});
+  });
 
 router.get('/download', function(req, res){
 	var src = req.param('src', null);
@@ -140,7 +205,7 @@ router.post('/upsert-student', function(req, res, next) {
     			console.log(err);
     		}
     	});
-});
+	});
 
 	res.redirect('student-search');
 });
--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 sudo apt-get update;
-sudo apt-get install nodejs;
+sudo apt-get install nodejs -y;
 sudo apt-get install nodejs-legacy -y;
 sudo apt-get install npm -y;
 sudo apt-get install postgresql-9.5 -y;
+sudo apt-get install pdftk -y;
+sudo apt-get install poppler-utils -y;
+sudo apt-get install ghostscript -y;
+sudo apt-get install tesseract-ocr -y;
 
 sudo npm install;
-
-sudo mkdir uploaded_files;
-sudo mkdir bill_files;
 
 sudo adduser --quiet --disabled-password --gecos "Ivonete DB" ivonetedb
 echo "ivonetedb:abcdef" | sudo chpasswd
@@ -17,17 +18,10 @@ sudo runuser -l postgres -c "psql -c \"DROP DATABASE "ivonetedb"\"";
 sudo runuser -l postgres -c "psql -c \"CREATE DATABASE "ivonetedb" OWNER "ivonetedb";\"";
 
 sudo runuser -l ivonetedb -c "psql -c \"
-CREATE TABLE student(
-	cpf VARCHAR(14) PRIMARY KEY,
-	name VARCHAR(128)
-)\"";
-
-sudo runuser -l ivonetedb -c "psql -c \"
 CREATE TABLE bill(
 	year INT,
 	month INT,
-	student_cpf VARCHAR(14) REFERENCES student,
-	value DECIMAL(12,2),
+	student_name VARCHAR(128),
 	src VARCHAR(256),
-	PRIMARY KEY(year, month, student_cpf)
+	PRIMARY KEY(year, month, student_name)
 )\"";
